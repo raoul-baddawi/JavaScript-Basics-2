1b3bc5748a15b9d0035f49c6f5775414af430b81
raoul-baddawi <raoulbaddawi1@gmail.com>
2022-12-09 01:44:01 +0200

    Step5 done

---
+++ step 5/Step5.js
@@ -0,0 +1,7 @@
+function mouseOver() {
+    document.getElementById("image1").src="./images/image1_2.jpg";
+    document.getElementById("image2").src="./images/image1_2.jpg";
+    document.getElementById("image3").src="./images/image1_2.jpg";
+    document.getElementById("image4").src="./images/image1_2.jpg";
+    document.getElementById("image5").src="./images/image1_2.jpg";
+}
