944df89060a93aacb6ebc7e143b2844b547c1c38
raoul-baddawi <raoulbaddawi1@gmail.com>
2022-12-08 10:10:47 +0200

    Step1 done

---
+++ step 1/Step1.js
@@ -0,0 +1,6 @@
+function setNewImage(){
+    document.querySelector("#image1").src="images/image1_2.jpg";
+}
+function setOldImage(){
+    document.querySelector("#image1").src="images/image1.jpg";
+}
